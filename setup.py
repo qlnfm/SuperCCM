665c29ffe5ec0875b4741c8248638542a5eb4ffa
SummerColdWind <82140705+SummerColdWind@users.noreply.github.com>
2025-09-23 18:25:51 +0800

    Update read module

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def parse_requirements(filename):
 
 setup(
     name='superccm',
-    version='0.2.2',
+    version='0.2.3',
     author='Qincheng Qiao',
     author_email='jugking6688@gmail.com',
     description='An Open-Source Python Toolkit for Automated Quantification of Corneal Nerve Fibers in Confocal Microscopy Images',
